544bf608975f5fe34340a2e575c24eee7b80c817
uriel <urielsplenser@gmail.com>
2022-08-09 21:40:43 -0300

    modificacion ejercicio 14

--- 37/14.js
+++ 37/14.js
@@ -1,13 +1,22 @@
 /* 14) Programa una función para convertir grados Celsius a Fahrenheit y
 viceversa, pe. miFuncion(0,"C") devolverá 32°F. */
 
-const aFahrenheit = num => {
+const convertirTemperatura = (num, farCel) => {
     if(typeof num === 'number'){
-      return num * 1.8 + 32;
+      if(farCel === 'F'){
+        return num * 1.8 + 32;
+      }else if(farCel === 'C'){
+        return (num - 32) / 1.8;
+      }else{
+        return 'Debes poner F o C';
+      }
     }else{
       return 'No es un número';
     }
   }
   
-  console.log(aFahrenheit(12));
-  console.log(aFahrenheit(0));
+  console.log(convertirTemperatura(12, 'F'));
+  console.log(convertirTemperatura(0, 'F'));
+
+  console.log(convertirTemperatura(53.6, 'C'));
+  console.log(convertirTemperatura(32, 'C'));
